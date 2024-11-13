71d635788a6e8c0b2b2726138d6d8d828dce5d0a
rangayaswanth <yaswanthranga88@gmail.com>
2024-11-12 18:34:30 -0600

    All the files added

---
+++ src/Oct/ex_10202024/Strings.java
@@ -0,0 +1,15 @@
+package Oct.ex_10202024;
+
+public class Strings {
+    public static void main(String[] args) {
+        String name = "Rahul";
+        String name1 = "Rahul                        ";
+//        String name2 = new String("Rahul");
+//        System.out.println(name == name1); // true
+//        System.out.println(name == name2); // false
+//        System.out.println(name.equals(name2)); // true
+//        System.out.println(name.equalsIgnoreCase(name1));
+        System.out.println(name1.trim());
+
+    }
+}
